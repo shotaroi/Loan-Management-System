3f1750fff5a8a60dd218631288ce378fda20b5f2
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-02-21 04:56:36 +0100

    Create SecurityUser.java

---
+++ src/main/java/com/shotaroi/loan/security/SecurityUser.java
@@ -0,0 +1,68 @@
+package com.shotaroi.loan.security;
+
+import com.shotaroi.loan.common.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SecurityUser implements UserDetails {
+
+    private final Long id;
+    private final String email;
+    private final String passwordHash;
+    private final Role role;
+
+    public SecurityUser(Long id, String email, String passwordHash, Role role) {
+        this.id = id;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return passwordHash;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
